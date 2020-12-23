a958d3a318700142e758f6082899cc0436bce295
Nick Tzaperas <nick@nijotz.com>
2020-12-23 11:51:09 -0800

    Test v2 and v3 of fastify

--- fastify/index.js
+++ fastify/index.js
@@ -1,7 +1,33 @@
+const semver = require('semver')
+
+const fastifyVersion = require('./node_modules/fastify/package.json').version
+const v2 = semver.lt(fastifyVersion, '3.0.0')
+
+if (v2) {
+  process.env['NEW_RELIC_APP_NAME'] = 'fastify-test-v2'
+} else {
+  process.env['NEW_RELIC_APP_NAME'] = 'fastify-test-v3'
+}
+
 require('newrelic')
 const fastify = require('fastify')({
   logger: true
 })
+
+function loadMiddleware() {
+  fastify.use(require('cors')())
+  fastify.use(require('dns-prefetch-control')())
+  fastify.use(require('frameguard')())
+  fastify.use(require('hide-powered-by')())
+  fastify.use(require('hsts')())
+  fastify.use(require('ienoopen')())
+  fastify.use(require('x-xss-protection')())
+}
+
+if (v2) {
+  console.log('v2 middleware')
+  loadMiddleware()
+}
 
 fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
@@ -9,8 +35,11 @@ fastify.get('/', async (request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.register(require('fastify-express'))
-    fastify.use(require('cors')())
+    if (!v2) {
+      console.log('v3 middleware')
+      await fastify.register(require('fastify-express'))
+      loadMiddleware()
+    }
 
     await fastify.listen(3000)
   } catch (err) {
